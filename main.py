22295019d34ce95ffcace083be141188f682a26e
Andrey Zashchepin <razlivnoy@icloud.com>
2024-12-21 15:01:53 +0500

    Пофиксил класс работы с файлами

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from src.vacancies import Vacancy
 hh_api = HeadHunterAPI()
 
 # Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies("Python")
+hh_api.get_vacancies("Python")
 
 # Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.vacancy_from_list(hh_vacancies)
+vacancies_list = Vacancy.vacancy_from_list(hh_api.vacancies)
 
 # Пример работы конструктора класса с одной вакансией
 vacancy = Vacancy("Python Developer", "Москва", 100000, 150000, "https://hh.ru/vacancy/111348872")
@@ -17,24 +17,26 @@ vacancy = Vacancy("Python Developer", "Москва", 100000, 150000, "https://h
 # Сохранение информации о вакансиях в файл
 json_saver = JsonVacancy()
 json_saver.add_vacancy(vacancy)
+print(json_saver.load_vacancies())
+print(type(json_saver.load_vacancies()))
 json_saver.delete_vacancy(vacancy)
-
-# Функция для взаимодействия с пользователем
-def user_interaction():
-    platforms = ["HeadHunter"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
-
-    filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-
-    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-
-    sorted_vacancies = sort_vacancies(ranged_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
-
-
-if __name__ == "__main__":
-    user_interaction()
+#
+# # Функция для взаимодействия с пользователем
+# def user_interaction():
+#     platforms = ["HeadHunter"]
+#     search_query = input("Введите поисковый запрос: ")
+#     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+#     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+#     salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
+#
+#     filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
+#
+#     ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
+#
+#     sorted_vacancies = sort_vacancies(ranged_vacancies)
+#     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+#     print_vacancies(top_vacancies)
+#
+#
+# if __name__ == "__main__":
+#     user_interaction()
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,17 @@
 import json
 import os.path
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Any
 
 from src.vacancies import Vacancy
 
 
 class BaseVacancyFile(ABC):
-    """ Базовый класс для работы с файлами вакансий """
+    """Базовый класс для работы с файлами вакансий"""
 
     @abstractmethod
-    def load_vacancies(self) -> List[Vacancy]:
-        """ Получение списка объектов Vacancy из файла JSON """
+    def load_vacancies(self) -> Any:
+        """Получение списка объектов Vacancy из файла JSON"""
         pass
 
     @abstractmethod
@@ -26,34 +26,41 @@ class BaseVacancyFile(ABC):
 
 
 class JsonVacancy(BaseVacancyFile):
-    """ Класс для работы с вакансиями в формате JSON """
+    """Класс для работы с вакансиями в формате JSON"""
 
-    def __init__(self, filename: str = 'vacancies.json'):
-        """ Конструктор класса """
+    def __init__(self, filename: str = "vacancies.json") -> None:
+        """Конструктор класса"""
         self.__filename = filename
-        self.path_to_file = os.path.join(os.path.dirname(__file__), '..', 'data', self.__filename)
+        self.path_to_file = os.path.join(os.path.dirname(__file__), "..", "data", self.__filename)
 
-    def load_vacancies(self) -> List[Vacancy]:
-        """ Получение списка объектов Vacancy из файла JSON """
+    def load_vacancies(self) -> Any:
+        """Получение списка объектов Vacancy из файла JSON"""
         try:
-            with open(self.path_to_file, 'r', encoding='utf-8') as f:
+            with open(self.path_to_file, "r", encoding="utf-8") as f:
                 data = json.load(f)
-                return Vacancy.vacancy_from_list(data)
+                return data
         except FileNotFoundError:
             return []
         except json.JSONDecodeError:
             return []
 
     def add_vacancy(self, vacancy: Vacancy) -> None:
-        """ Добавление вакансии в файл JSON """
+        """Добавление вакансии в файл JSON"""
         vacancies = self.load_vacancies()
-        if not any(vac.__name == vacancy.__name and vac.__url == vacancy.__url for vac in vacancies):
-            with open(self.path_to_file, 'a', encoding='utf-8') as f:
-                json.dump(vacancy.to_dict(), f, ensure_ascii=False, indent=4)
+        vacancy = vacancy.to_dict()
+        if not any(
+            vac.get("name") == vacancy.get("name") and vac.get("url") == vacancy.get("url") for vac in vacancies
+        ):
+            vacancies.append(vacancy)
+            with open(self.path_to_file, "w", encoding="utf-8") as f:
+                json.dump(vacancies, f, ensure_ascii=False, indent=4)
 
     def delete_vacancy(self, vacancy: Vacancy) -> None:
-        """ Удаление вакансии из файла JSON """
+        """Удаление вакансии из файла JSON"""
         vacancies = self.load_vacancies()
-        new_data = [vac for vac in vacancies if vac.__url != vacancy.__url]
-        with open(self.path_to_file) as f:
-            json.dump([data.to_dict() for data in new_data], f, ensure_ascii=False, indent=4)
+        vacancy = vacancy.to_dict()
+        new_data = [
+            vac for vac in vacancies if vac.get("name") != vacancy.get("name") and vac.get("url") != vacancy.get("url")
+        ]
+        with open(self.path_to_file, "w", encoding="utf-8") as f:
+            json.dump(new_data, f, ensure_ascii=False, indent=4)
